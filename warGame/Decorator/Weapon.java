c00981e486932228b01043476381b044013fad49
Sibo-WANG <wang_921209@163.com>
2017-04-08 07:50:35 -0400

    weapon decorator

--- warGame/Decorator/Weapon.java
+++ warGame/Decorator/Weapon.java
@@ -1,7 +1,15 @@
 package warGame.Decorator;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Observable;
 
-public abstract class Weapon {
+public abstract class Weapon extends Observable{
 	public abstract ArrayList<String> addToEnchantList(String enchantment);
+	public abstract void saveWeapon(WeaponDecorator weapon) throws IOException;
+	public abstract Weapon adaptToLevel(int level);
+	public abstract void setWeaponView();
+	public abstract String lastWeaponID() throws UnsupportedEncodingException, FileNotFoundException;
 }
